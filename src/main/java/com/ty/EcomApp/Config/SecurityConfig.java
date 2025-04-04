29168a70b772521049257c82aa3846dd9198f0a0
Abhijith-02 <69385258+tron01@users.noreply.github.com>
2025-04-05 00:35:01 +0530

    added AuthenticationProvider
    AuthenticationProvider with plain password

--- src/main/java/com/ty/EcomApp/Config/SecurityConfig.java
+++ src/main/java/com/ty/EcomApp/Config/SecurityConfig.java
@@ -1,20 +1,41 @@
 package com.ty.EcomApp.Config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import com.ty.EcomApp.Service.CustomUserDetailServices;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 	
+	private final CustomUserDetailServices services;
+	
+	
+	public SecurityConfig(CustomUserDetailServices services) {
+		this.services = services;
+	}
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.formLogin(form -> form
-				.loginPage("/login"));
+		http.formLogin(form -> form.loginPage("/login"));
 		return http.build();
 	}
+	
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		//plain password
+		provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+		provider.setUserDetailsService(services);
+		return provider;
+	}
+	
 	
 }
