9712087c292bb0ed4880ccbe086f36082a8d98a0
Abhijith-02 <69385258+tron01@users.noreply.github.com>
2025-04-05 00:33:52 +0530

    Create CustomUserDetailServices.java
     CustomUserDetailServices implements UserDetailsService

---
+++ src/main/java/com/ty/EcomApp/Service/CustomUserDetailServices.java
@@ -0,0 +1,29 @@
+package com.ty.EcomApp.Service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.ty.EcomApp.Model.CustomUserDetail;
+import com.ty.EcomApp.Model.User;
+import com.ty.EcomApp.Repository.UserRepository;
+
+public class CustomUserDetailServices implements UserDetailsService{
+
+	private final UserRepository repository;
+	
+	public CustomUserDetailServices(UserRepository repository) {
+		this.repository = repository;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = repository.findByUsername(username);
+		if(user!=null) {
+			System.out.println("user not found!");
+			throw new UsernameNotFoundException("User not Found");
+		}
+		return new CustomUserDetail(user);
+	}
+
+}
