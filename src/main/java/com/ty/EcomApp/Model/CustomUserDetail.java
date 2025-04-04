f87983385e65fed759d6e16a904de9fd0150a80f
Abhijith-02 <69385258+tron01@users.noreply.github.com>
2025-04-05 00:16:06 +0530

    CustomUserDetail in model
    CustomUserDetail implements  UserDetails

---
+++ src/main/java/com/ty/EcomApp/Model/CustomUserDetail.java
@@ -0,0 +1,33 @@
+package com.ty.EcomApp.Model;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class CustomUserDetail implements UserDetails {
+
+	private final User user;
+	
+	public CustomUserDetail(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.singleton(new SimpleGrantedAuthority(user.getRoles()));
+	}
+
+	@Override
+	public String getPassword() {
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return user.getUsername();
+	}
+
+}
