cdb0d0152c5248f18195d6a35d790516cf81c636
Abhijith-02 <69385258+tron01@users.noreply.github.com>
2025-04-05 20:19:04 +0530

    Error controller and error.html

---
+++ src/main/java/com/ty/EcomApp/Controller/CustomErrorController.java
@@ -0,0 +1,21 @@
+package com.ty.EcomApp.Controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @GetMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        model.addAttribute("status", status);
+        model.addAttribute("message", "Something went wrong!");
+        return "error"; // Returns error.html
+    }
+}
