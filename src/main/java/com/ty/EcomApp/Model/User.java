8405201fd1b3af8493e2e9bcde0cd3d1f03af855
Abhijith-02 <69385258+tron01@users.noreply.github.com>
2025-04-06 17:04:44 +0530

    confirmPassword  field  @User Model
    @Transient  This field won't be saved in the database

--- src/main/java/com/ty/EcomApp/Model/User.java
+++ src/main/java/com/ty/EcomApp/Model/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
 
 @Entity
 public class User {
@@ -13,11 +14,19 @@ public class User {
 	private String username;
 	private String password;
 	private String roles;
+	@Transient  // This field won't be saved in the database
+    private String confirmPassword;
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
+	}
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
 	}
 	public String getUsername() {
 		return username;
