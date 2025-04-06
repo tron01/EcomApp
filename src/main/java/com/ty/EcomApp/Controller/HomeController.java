6c7230cbaf709dc50c1cb17b1526ce711da854db
Abhijith-02 <69385258+tron01@users.noreply.github.com>
2025-04-06 17:04:06 +0530

    signup added
    @GetMapping("/signup")
    @PostMapping("/signup")

--- src/main/java/com/ty/EcomApp/Controller/HomeController.java
+++ src/main/java/com/ty/EcomApp/Controller/HomeController.java
@@ -8,12 +8,26 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.ty.EcomApp.Model.User;
+import com.ty.EcomApp.Repository.UserRepository;
+import com.ty.EcomApp.Service.UserService;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
+
+	private final UserService service;
+	private final UserRepository repository;
+
+	public HomeController(UserService service, UserRepository repository) {
+		this.service = service;
+		this.repository = repository;
+	}
+
 
 	@GetMapping("/")
 	private String home(Model model ,Principal principal) {
@@ -30,6 +44,7 @@ public class HomeController {
 	     model.addAttribute("role", role);
 		return "home";
 	}
+	
 	@GetMapping("/login")
 	String login(HttpServletRequest request) {
 		// If user is already authenticated, redirect to home
@@ -40,5 +55,30 @@ public class HomeController {
         return "login";
 	}
 	
+	@GetMapping("/signup")
+	private String signup(Model model) {
+		 // Ensure 'user' is added to the model for using thyemleaf -> th:object="${user}" on post request. 
+		 model.addAttribute("user", new User()); 
+	 return "signup";
+	}
+	
+	@PostMapping("/signup")
+	private String create_user(@ModelAttribute User user, Model model) {
+		
+		if(!user.getPassword().equals(user.getConfirmPassword())) {
+            model.addAttribute("error", "Passwords do not match!");
+            return "signup";
+        }
+
+        if(repository.findByUsername(user.getUsername()) != null) {
+            model.addAttribute("error", "Username already exists!");
+            return "signup";
+        }
+        User u1=service.create_user(user);
+		System.out.println(u1);
+        return "redirect:/login"; // Redirect to login page with success message
+		
+		
+	}	
 
 }
