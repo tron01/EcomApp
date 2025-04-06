2904ec84a1d8472415178bc2b40a8874774a6a7a
Abhijith-02 <69385258+tron01@users.noreply.github.com>
2025-04-06 23:21:48 +0530

    User Crud Operations

--- src/main/java/com/ty/EcomApp/Controller/AdminController.java
+++ src/main/java/com/ty/EcomApp/Controller/AdminController.java
@@ -6,6 +6,9 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ty.EcomApp.Model.CustomUserDetail;
@@ -31,10 +34,32 @@ public class AdminController {
 	}
 	
 
-    @GetMapping("/user_list")
+    @GetMapping("/users")
     public String userList(Model model) {
         List<User> users = (List<User>) userRepository.findAll();
         model.addAttribute("users", users);
         return "user_list"; 
+    }
+    
+    // Show edit user form
+    @GetMapping("/edit/{id}")
+    public String editUser(@PathVariable int id, Model model) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        model.addAttribute("user", user);
+        return "edituser"; 
+    }
+
+    //user update
+    @PostMapping("/User_update")
+    public String updateUser(@ModelAttribute User user, Model model) {
+        userRepository.save(user); // Save updated user data
+        return "redirect:/admin/users"; // Redirect to user list
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable int id) {
+        userRepository.deleteById(id);
+        return "redirect:/admin/users";
     }
 }
