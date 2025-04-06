9ffaf1b647a7d2ff50d74fffade020d8b7d24a47
Abhijith-02 <69385258+tron01@users.noreply.github.com>
2025-04-06 17:05:05 +0530

    create_user added on UserService

--- src/main/java/com/ty/EcomApp/Service/UserService.java
+++ src/main/java/com/ty/EcomApp/Service/UserService.java
@@ -1,5 +1,23 @@
 package com.ty.EcomApp.Service;
 
+import org.springframework.stereotype.Service;
+
+import com.ty.EcomApp.Model.User;
+import com.ty.EcomApp.Repository.UserRepository;
+
+@Service
 public class UserService {
 
+	private final UserRepository repository;
+	
+	public UserService(UserRepository repository) {
+		this.repository = repository;
+	}
+
+	public User create_user(User user) {
+		user.setRoles("USER");
+		return repository.save(user);	
+	}
+
+	
 }
